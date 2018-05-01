33355ac5d293c2227190d683eb1397729d636131
Valentinas Mirosnicenko <valentinasmirosnicenko@Valentinass-MacBook-Pro-5.local>
2018-05-01 10:14:56 +0800

    Lesson 6 completed

---
+++ PixelCity/View/DroppablePin.swift
@@ -0,0 +1,22 @@
+//
+//  DroppablePin.swift
+
+//  PixelCity
+//
+//  Created by Valentinas Mirosnicenko on 5/1/18.
+//  Copyright © 2018 Valentinas Mirosnicenko. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class DroppablePin: NSObject, MKAnnotation {
+    dynamic var coordinate: CLLocationCoordinate2D
+    var identifier: String
+    
+    init(coordinate: CLLocationCoordinate2D, identifier: String) {
+        self.coordinate = coordinate
+        self.identifier = identifier
+        super.init()
+    }
+}
