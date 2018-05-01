ce34766ed8fb553535487e4cd008e7a8d52ebcfb
Valentinas Mirosnicenko <valentinasmirosnicenko@Valentinass-MacBook-Pro-5.local>
2018-05-01 14:30:56 +0800

    Lesson 10 Completed

--- PixelCity/Utilities/Constants.swift
+++ PixelCity/Utilities/Constants.swift
@@ -7,3 +7,16 @@
 //
 
 import Foundation
+
+
+let API_KEY = "b5512604dec20be7bc9e2c0df74958ee"
+let SECRET = "1aabf3b3ae09522f"
+
+func flickerUrl(withAnotation annotation: DroppablePin, numberOfPhotos number: Int) -> String {
+    
+    let url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=\(API_KEY)&lat=\(annotation.coordinate.latitude)&lon=\(annotation.coordinate.longitude)&radius=5&radius_units=mi&per_page=\(number)&format=json&nojsoncallback=1"
+   
+    return url
+}
+
+
