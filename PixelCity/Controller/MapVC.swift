5af331547e0c173b2dc7a1b14150b16226836f6e
Valentinas Mirosnicenko <valentinasmirosnicenko@Valentinass-MacBook-Pro-5.local>
2018-05-01 14:55:35 +0800

    Lesson 11 completed

--- PixelCity/Controller/MapVC.swift
+++ PixelCity/Controller/MapVC.swift
@@ -9,6 +9,8 @@
 import UIKit
 import MapKit
 import CoreLocation
+import Alamofire
+import AlamofireImage
 
 class MapVC: UIViewController, UIGestureRecognizerDelegate {
 
@@ -27,6 +29,7 @@ class MapVC: UIViewController, UIGestureRecognizerDelegate {
     let authorizationStatus = CLLocationManager.authorizationStatus()
     let regionRadius: Double = 1000
     var screenSize = UIScreen.main.bounds
+    var imageUrlArray = [String]()
     
     
     override func viewDidLoad() {
@@ -119,6 +122,37 @@ class MapVC: UIViewController, UIGestureRecognizerDelegate {
         }
     }
     
+    func retrieveUrls(forAnnotation annotation:DroppablePin, completion: @escaping (_ status: Bool) -> ()) {
+        imageUrlArray = []
+        
+        Alamofire.request(flickerUrl(withAnotation: annotation, numberOfPhotos: 40)).responseJSON { (response) in
+            
+            guard let json = response.result.value as? Dictionary<String, AnyObject> else {
+                debugPrint("Failed turned into json")
+                return
+            }
+            
+            guard let photosDictionary = json["photos"] as? Dictionary<String, AnyObject> else {
+                debugPrint("failed to get photosDictionary")
+                return
+            }
+            
+            guard let photosDictionaryArray = photosDictionary["photo"] as? [Dictionary<String, AnyObject>] else {
+                debugPrint("Failed to receive any photos")
+                return
+            }
+            
+            for photo in photosDictionaryArray {
+                let postUrl = "https://farm\(photo["farm"]!).staticflickr.com/\(photo["server"]!)/\(photo["id"]!)_\(photo["secret"]!)_h_d.jpg"
+                self.imageUrlArray.append(postUrl)
+            }
+            
+            completion(true)
+        }
+        
+        
+    }
+    
 }
 
 extension MapVC: MKMapViewDelegate {
@@ -159,6 +193,12 @@ extension MapVC: MKMapViewDelegate {
         
         let coordinateRegion = MKCoordinateRegionMakeWithDistance(touchCoordinate, regionRadius*2, regionRadius*2)
         mapView.setRegion(coordinateRegion, animated: true)
+        
+        retrieveUrls(forAnnotation: annotation) { (success) in
+            if success {
+                print(self.imageUrlArray)
+            }
+        }
         
         print(touchPoint, touchCoordinate)
     }
--- PixelCity/Utilities/Constants.swift
+++ PixelCity/Utilities/Constants.swift
@@ -12,6 +12,8 @@ import Foundation
 let API_KEY = "b5512604dec20be7bc9e2c0df74958ee"
 let SECRET = "1aabf3b3ae09522f"
 
+// https://farm1.staticflickr.com/464/18584211154_cf9a7908aa_k_d.jpg
+
 func flickerUrl(withAnotation annotation: DroppablePin, numberOfPhotos number: Int) -> String {
     
     let url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=\(API_KEY)&lat=\(annotation.coordinate.latitude)&lon=\(annotation.coordinate.longitude)&radius=5&radius_units=mi&per_page=\(number)&format=json&nojsoncallback=1"
